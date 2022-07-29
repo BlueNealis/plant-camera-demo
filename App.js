9f8e55d7c9e006071dbe7339ce8adadd5fffdf78
Blue Jessen <canealisblue@gmail.com>
2022-07-29 10:44:15 -0700

    Add alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Button, Alert} from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import {styles} from './AppStyles'  //This is because Im using class so I have styles in a
 //separate file ^
@@ -15,38 +15,15 @@ class App extends Component {
     }
   }
 
-  openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if(!persmissionResult.granted) {
-      alert('Access to camera roll is required for the app to function as intended')
-      return
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync()
-    if(pickerResult.cancelled) {
-      return
-    }
-    this.setState({
-      selectedImage: {localUri: pickerResult.uri},
-    })
+handleClick = (event) => {
+    Alert.alert('WOAH', 'I cant believe youve done this')
   }
 
-  openCameraAsync = async () => {
-    let permissionResult = await ImagePicker.requestCameraPermissionsAsync()
-    if(!persmissionResult.granted) {
-      alert('Access to camera is required for the app to function as intended')
-      return
-    } else {
-    ImagePicker.launchCameraAsync()
-  }
-  }
 
   render() {
   return (
     <View style={styles.container}>
-      <Header />
-      <PickImage openImagePicker={this.openImagePickerAsync} openCamera={this.openCameraAsync}/>
-      <StatusBar style="auto" />
+      <Button onClick={this.handleClick} title='Press Me'/>
     </View>
   );
 }
